cde318f38cc32d190ce46cbc83fe3269a35d4093
Ryy Pascual <pascualryan134@gmail.com>
2024-02-02 22:45:14 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+const database = admin.database();
+
+let GLOBAL_COUNT = 0;
+
+exports.helloWorld = onRequest(async (request, response) => {
+    logger.info("Hello logs!", { structuredData: true });
+
+    // Save telemetry to Firebase Realtime Database
+    await database.ref('telemetry').push({
+        msg: `Hello #${GLOBAL_COUNT++}`,
+        timestamp: Date.now(),
+    });
+
+    response.send("Hello from Firebase!");
+});
